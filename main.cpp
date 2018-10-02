2dfb33a23e6ed65161d35c79e8f7c997d4a72adc
tagirmi <tagirmi@gmail.com>
2018-10-02 22:37:47 +0300

    2-мерная матрица с операциями получения актуального размера и добавления элементов

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cassert>
+
+#include "matrix.h"
+
+void test_matrix()
+{
+  hw06::Matrix<int, -1> matrix;
+  assert(matrix.size() == 0);
+
+  auto a = matrix[0][0];
+  assert(a == -1);
+  assert(matrix.size() == 0);
+
+  matrix[100][100] = 314;
+  assert(matrix[100][100] == 314);
+  assert(matrix.size() == 1);
+
+  matrix[100][100] = -1;
+  assert(matrix[100][100] == -1);
+  assert(matrix.size() == 0);
+}
 
 int main(int argc, char const *argv[])
 {
   try
   {
+    test_matrix();
+
 
   }
   catch(const std::exception& ex)
--- matrix.h
+++ matrix.h
@@ -1,1 +1,73 @@
 #pragma once
+
+#include <array>
+#include <map>
+
+namespace hw06 {
+
+template<typename T, T Def>
+class Matrix
+{
+public:
+  using Index = std::array<size_t, 2>;
+  using Data = std::map<Index, T>;
+
+  class Proxy
+  {
+  public:
+    Proxy(size_t index, Data& data)
+      : m_index{index, 0}
+      , m_data{data}
+    {
+    }
+
+    Proxy(const Index& index, Data& data)
+      : m_index{index}
+      , m_data{data}
+    {
+    }
+
+    auto operator [](size_t i)
+    {
+      m_index[1] = i;
+      return Proxy{m_index, m_data};
+    }
+
+    auto& operator =(const T& value)
+    {
+      if (value == Def) {
+        m_data.erase(m_index);
+        return m_default;
+      }
+      m_data[m_index] = value;
+    }
+
+    operator const T& () const
+    {
+      auto i = m_data.find(m_index);
+      return i == m_data.end() ? m_default : i->second;
+    }
+
+  private:
+    Index m_index;
+    Data& m_data;
+    T m_default{Def};
+  };
+
+  Matrix() = default;
+
+  size_t size() const
+  {
+    return m_data.size();
+  }
+
+  Proxy operator [](size_t index)
+  {
+    return Proxy{index, m_data};
+  }
+
+private:
+  Data m_data;
+};
+
+} // namespace
