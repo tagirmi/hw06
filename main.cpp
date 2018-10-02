46520f389bda5f6121303723860048602a7d1533
tagirmi <tagirmi@gmail.com>
2018-10-02 22:59:31 +0300

    заполнение матрицы согласно заданию

--- main.cpp
+++ main.cpp
@@ -19,6 +19,10 @@ void test_matrix()
   matrix[100][100] = -1;
   assert(matrix[100][100] == -1);
   assert(matrix.size() == 0);
+
+//  ((matrix[100][100] = 314) = 0) = 217;
+//  assert(matrix[100][100] == 217);
+//  assert(matrix.size() == 1);
 }
 
 int main(int argc, char const *argv[])
@@ -27,7 +31,31 @@ int main(int argc, char const *argv[])
   {
     test_matrix();
 
+    hw06::Matrix<int, 0> matrix;
 
+    for (int i = 0; i < 10; ++i) {
+      matrix[i][i] = i;
+      matrix[i][9 - i] = 9 - i;
+    }
+
+    std::cout << "Matrix:" << std::endl;
+    for (int i = 1; i < 9; ++i ) {
+      for (int j = 1; j < 9; ++j) {
+        std::cout << matrix[i][j] << ' ';
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << "\nMatrix size:" << std::endl;
+    std::cout << matrix.size() << std::endl;
+
+//    std::cout << "\nMatrix not empty cells:" << std::endl;
+//    for (auto& cell : matrix) {
+//      size_t i, j;
+//      int v;
+//      std::tie(i, j, v) = cell;
+//      std::cout << i << j << v << std::endl;
+//    }
   }
   catch(const std::exception& ex)
   {
